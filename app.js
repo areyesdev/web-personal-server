fe37e6234102891cbcc821fa049a42e7a882174c
Andres Reyes <ar97yf@gmail.com>
2020-06-10 23:20:16 -0500

    [ADD] menu web en el admin

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const { API_VERSION } = require('./config');
 //Load routings
 const authRoutes = require('./routers/auth');
 const userRoutes = require('./routers/user');
+const menuRoutes = require('./routers/menu');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -26,5 +27,6 @@ app.use((req, res, next) => {
 //Router Basic
 app.use(`/api/${API_VERSION}`, authRoutes);
 app.use(`/api/${API_VERSION}`, userRoutes);
+app.use(`/api/${API_VERSION}`, menuRoutes);
 
 module.exports = app;
